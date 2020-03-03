e401c0ad50d9e4241434e4ca743b381bf5af63d6
abhijay24 <abhijay24.3.7@gmail.com>
2020-03-03 20:34:51 +0530

    platform1

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var platform1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -18,6 +19,7 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
+    platform1 = new Ground(150, 200, 15, 10);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -35,6 +37,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
+
 
 }
 
@@ -61,4 +65,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    platform1.display();
 }
